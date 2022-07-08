6a2389594ba790d5e2cd7101b5112a81d7d035d1
zmigliorini <zmigliorini@gmail.com>
2022-07-07 20:28:57 -0400

    Adding support for hibernate

--- gpp/src/main/java/com/zmigliorini/gpp/Controller.java
+++ gpp/src/main/java/com/zmigliorini/gpp/Controller.java
@@ -3,6 +3,7 @@ package com.zmigliorini.gpp;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	@Autowired
+	private CountryRepository countryRepo;
 	
 	private final String url = "jdbc:mysql://localhost:3306/gpp";
-	private final String username = "admin";
+	private final String username = "java";
 	private final String password = "password";
 	
 	private DBConn conn = new DBConn(url, username, password);
@@ -39,55 +42,55 @@ public class Controller {
 			value = "/country"
 	)
 	public Country getCountry(@RequestParam(value="name") String name) { 
-		Country country = null;
-		ResultSet set = null;
+//		Country country = null;
+//		ResultSet set = null;
+//		
+//		//Replace underlines that client is instructed to use, and trim whitespace.
+//		name = name.replace("_", " ").trim();
+//		
+//		try {
+//			conn.connectDB();
+//		} catch (SQLException e) {
+//			System.err.println("SQLException on db connection attempt.");
+//			System.err.println("Error Code = " + e.getErrorCode());
+//			System.err.println("SQL state = " + e.getSQLState());
+//			System.err.println("Message = " + e.getMessage());
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			set = conn.queryCountry(name);
+//		} catch (SQLException e) {
+//			System.err.println("SQLException on query attempt.");
+//			System.err.println("Error Code = " + e.getErrorCode());
+//			System.err.println("SQL state = " + e.getSQLState());
+//			System.err.println("Message = " + e.getMessage());
+//			e.printStackTrace();
+//		}
+//		
+//		//Handle null ResultSet by returning null to Jackson parser
+//		if (set == null) {
+//			System.err.println("ResultSet null after query. Returning null response");
+//			return null;
+//		}
+//		
+//		try {
+//			country = new Country(set);
+//		} catch (IllegalArgumentException e) {
+//			System.err.println("Caught IllegalArgumentException when calling constructor Country(ResultSet set).");
+//			e.printStackTrace();
+//		}
+//		
+//		//If somehow country object still null after constructor
+//		if (country == null) {
+//			System.err.println("No exception but Country object still null after constructor call.\n Returning null to Jackson.");
+//			return null;
+//		}
+//		
+//		
+//		return country;
 		
-		//Replace underlines that client is instructed to use, and trim whitespace.
-		name = name.replace("_", " ").trim();
-		
-		try {
-			conn.connectDB();
-		} catch (SQLException e) {
-			System.err.println("SQLException on db connection attempt.");
-			System.err.println("Error Code = " + e.getErrorCode());
-			System.err.println("SQL state = " + e.getSQLState());
-			System.err.println("Message = " + e.getMessage());
-			e.printStackTrace();
-		}
-		
-		try {
-			set = conn.queryCountry(name);
-		} catch (SQLException e) {
-			System.err.println("SQLException on query attempt.");
-			System.err.println("Error Code = " + e.getErrorCode());
-			System.err.println("SQL state = " + e.getSQLState());
-			System.err.println("Message = " + e.getMessage());
-			e.printStackTrace();
-		}
-		
-		//Handle null ResultSet by returning null to Jackson parser
-		if (set == null) {
-			System.err.println("ResultSet null after query. Returning null response");
-			return null;
-		}
-		
-		try {
-			country = new Country(set);
-		} catch (IllegalArgumentException e) {
-			System.err.println("Caught IllegalArgumentException when calling constructor Country(ResultSet set).");
-			e.printStackTrace();
-		}
-		
-		//If somehow country object still null after constructor
-		if (country == null) {
-			System.err.println("No exception but Country object still null after constructor call.\n Returning null to Jackson.");
-			return null;
-		}
-		
-		country.getAvgElevationMeters();
-		//confirm not null lol
-		
-		return country;
+		return null;
 		
 	}
 	
--- gpp/src/main/java/com/zmigliorini/gpp/Country.java
+++ gpp/src/main/java/com/zmigliorini/gpp/Country.java
@@ -1,210 +1,139 @@
 package com.zmigliorini.gpp;
 
-import java.sql.SQLException;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-
-
-@JsonIgnoreProperties(value = {"numColumns"})
+@Entity
+@Table(name="countries")
 public class Country {
 	
-	//Number of columns a record in gpp.countries table must have
-	private final int numColumns = 19;
+	public Country () {}
 	
-	private final String name;
-	private final int popM;
-	private final int sizeKM2;
-	private final double birthRate;
-	private final int avgElevationMeters;
-	private final boolean isLandlocked;
-	private final int gdpM;
-	private final int importM;
-	private final int exportM;
-	private final int standingArmyK;
-	private final int navalDispTons;
-	private final int numWarplanes;
-	private final String continent;
-	private final String religion;
-	private final String ethnicity;
-	private final String language;
-	private final String government;
-	private final String tradeBloc;
-	private final String milAlliance;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 	
-	/**
-	 * 
-	 * @param set A non-null ResultSet object consisting of a header and one record representing a country 
-	 * 	from 'countries' table in 'gpp' database. 
-	 */
-	public Country(ResultSet set) throws IllegalArgumentException {
-		try {
-			set.next(); //Skip the header
-			ResultSetMetaData rsmd = set.getMetaData();
-			
-			//Confirm correct number of columns in set, throw IllegalArgumentException otherwise
-			if (rsmd.getColumnCount() != this.numColumns) {
-				throw new IllegalArgumentException("ResultSet object contains " + rsmd.getColumnCount() 
-					+ " number of columns, Country constructor expecting " + this.numColumns + " number of columns.");
-			}
-			
-			//set is valid, instantiate Country object
-			this.name = set.getString(1);
-			this.popM = set.getInt(2);
-			this.sizeKM2 = set.getInt(3);
-			this.birthRate = set.getDouble(4);
-			this.avgElevationMeters = set.getInt(5);
-			this.isLandlocked = set.getBoolean(6);
-			this.gdpM = set.getInt(7);
-			this.importM = set.getInt(8);
-			this.exportM = set.getInt(9);
-			this.standingArmyK = set.getInt(10);
-			this.navalDispTons = set.getInt(11);
-			this.numWarplanes = set.getInt(12);
-			this.continent = set.getString(13);
-			this.religion = set.getString(14);
-			this.ethnicity = set.getString(15);
-			this.language = set.getString(16);
-			this.government = set.getString(17);
-			this.tradeBloc = set.getString(18);
-			this.milAlliance = set.getString(19);
-			
-		} catch (SQLException e) {
-			System.err.println("Caught SQLException in Country(ResultSet set) constructor.");
-			System.err.println("Error Code = " + e.getErrorCode());
-			System.err.println("SQL state = " + e.getSQLState());
-			System.err.println("Message = " + e.getMessage());
-			e.printStackTrace();
-			throw new IllegalArgumentException("Error attempting to parse ResultSet set in Country constructor");
-		}
+	@Column(name="name", length=100)
+	private String name;
+	
+	@Column(name="pop_m")
+	private Integer popM;
+	
+	@Column(name="size_km2")
+	private Integer sizeKM2;
+	
+	@Column(name="birth_rate")
+	private Float birthRate;
+	
+	@Column(name="avg_elevation_meters")
+	private Integer avgElevationMeters;
+	
+	@Column(name="is_landlocked")
+	private Boolean isLandlocked;
+
+	@Column(name="gdp_m")
+	private Integer gdpM;
+	
+	@Column(name="import_m")
+	private Integer importM;
+	
+	@Column(name="export_m")
+	private Integer exportM;
+	
+	@Column(name="standing_army_k")
+	private Integer standingArmyK;
+	
+	@Column(name="naval_disp_tons")
+	private Integer navalDispTons;
+	
+	@Column(name="num_warplanes")
+	private Integer numWarplanes;
+	
+	@Column(name="continent")
+	private String continent;
+	
+	@Column(name="religion")
+	private String religion;
+	
+	@Column(name="ethnicity")
+	private String ethnicity;
+	
+	@Column(name="language")
+	private String language;
+	
+	@Column(name="government")
+	private String government;
+	
+	@Column(name="trade_bloc")
+	private String tradeBloc;
+	
+	@Column(name="mil_alliance")
+	private String milAlliance;
+	
+	public void setId(Integer id) {
+		this.id = id;
 	}
-	
-	public Country(String name, int popM, int sizeKM2, double birthRate, int avgElevationMeters,
-			boolean isLandlocked, int gdpM, int importM, int exportM, int standingArmyK, int navalDispTons,
-			int numWarplanes, String continent, String religion, String ethnicity, String language, String government,
-			String tradeBloc, String milAlliance) {
-		super();
+	public void setName(String name) {
 		this.name = name;
+	}
+	public void setPopM(Integer popM) {
 		this.popM = popM;
+	}
+	public void setSizeKM2(Integer sizeKM2) {
 		this.sizeKM2 = sizeKM2;
+	}
+	public void setBirthRate(Float birthRate) {
 		this.birthRate = birthRate;
+	}
+	public void setAvgElevationMeters(Integer avgElevationMeters) {
 		this.avgElevationMeters = avgElevationMeters;
+	}
+	public void setIsLandlocked(Boolean isLandlocked) {
 		this.isLandlocked = isLandlocked;
+	}
+	public void setGdpM(Integer gdpM) {
 		this.gdpM = gdpM;
+	}
+	public void setImportM(Integer importM) {
 		this.importM = importM;
+	}
+	public void setExportM(Integer exportM) {
 		this.exportM = exportM;
+	}
+	public void setStandingArmyK(Integer standingArmyK) {
 		this.standingArmyK = standingArmyK;
+	}
+	public void setNavalDispTons(Integer navalDispTons) {
 		this.navalDispTons = navalDispTons;
+	}
+	public void setNumWarplanes(Integer numWarplanes) {
 		this.numWarplanes = numWarplanes;
+	}
+	public void setContinent(String continent) {
 		this.continent = continent;
+	}
+	public void setReligion(String religion) {
 		this.religion = religion;
+	}
+	public void setEthnicity(String ethnicity) {
 		this.ethnicity = ethnicity;
+	}
+	public void setLanguage(String language) {
 		this.language = language;
+	}
+	public void setGovernment(String government) {
 		this.government = government;
+	}
+	public void setTradeBloc(String tradeBloc) {
 		this.tradeBloc = tradeBloc;
+	}
+	public void setMilAlliance(String milAlliance) {
 		this.milAlliance = milAlliance;
 	}
-	
-	public int getNumColumns() {
-		return this.numColumns;
-	}
-
-	
-	public String getName() {
-		return name;
-	}
-
-
-	public int getPopM() {
-		return popM;
-	}
-
-
-	public int getSizeKM2() {
-		return sizeKM2;
-	}
-
-
-	public double getBirthRate() {
-		return birthRate;
-	}
-
-
-	public int getAvgElevationMeters() {
-		return avgElevationMeters;
-	}
-
-
-	public boolean getIsLandlocked() {
-		return isLandlocked;
-	}
-
-
-	public int getGdpM() {
-		return gdpM;
-	}
-
-
-	public int getImportM() {
-		return importM;
-	}
-
-
-	public int getExportM() {
-		return exportM;
-	}
-
-
-	public int getStandingArmyK() {
-		return standingArmyK;
-	}
-
-
-	public int getNavalDispTons() {
-		return navalDispTons;
-	}
-
-
-	public int getNumWarplanes() {
-		return numWarplanes;
-	}
-
-
-	public String getContinent() {
-		return continent;
-	}
-
-
-	public String getReligion() {
-		return religion;
-	}
-
-
-	public String getEthnicity() {
-		return ethnicity;
-	}
-
-
-	public String getLanguage() {
-		return language;
-	}
-
-
-	public String getGovernment() {
-		return government;
-	}
-
-
-	public String getTradeBloc() {
-		return tradeBloc;
-	}
-
-
-	public String getMilAlliance() {
-		return milAlliance;
-	}
-
-	
+		
 }
